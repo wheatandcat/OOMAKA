c6a414ee663c41db5ee306c6b0c01d5db770e99b
wheatandcat <wheatandcat@gmail.com>
2023-09-17 20:45:28 +0900

    fix

--- src/features/schedules/components/InputItem.tsx
+++ src/features/schedules/components/InputItem.tsx
@@ -1,19 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import { DatePicker, type DateValue } from "@mantine/dates";
 import { Modal } from "@mantine/core";
 
-const InputItem = () => {
+type Props = {
+  date?: DateValue;
+  emoji?: string;
+  value?: string;
+  input?: boolean;
+};
+
+const InputItem = (props: Props) => {
+  const [value, setValue] = useState(props.value ?? "");
   const [isOpen, setIsOpen] = useState(false);
   const [date, setDate] = useState<DateValue>(null);
 
+  const onKeyDown = useCallback((e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      console.log("Enter");
+    }
+  }, []);
+
   return (
-    <div className="flex items-center">
-      <div className="relative block h-8">
+    <div className="input-item flex items-center">
+      <div className="relative block h-6">
         <span
-          className="absolute top-1/2 h-6 w-8 -translate-y-1/2 transform cursor-pointer"
+          className="absolute top-1/2 h-4 w-8 -translate-y-1/2 transform cursor-pointer"
           onClick={() => setIsOpen(true)}
         >
-          {date ? date.getDate() : "🗓️"}
+          {(() => {
+            if (date) {
+              return date.getDate();
+            } else {
+              if (props.emoji) {
+                return props.emoji;
+              }
+              return "🗓️";
+            }
+          })()}
         </span>
         <Modal opened={isOpen} onClose={() => setIsOpen(false)}>
           <div className="mb-5 flex items-center justify-center">
@@ -29,8 +52,12 @@ const InputItem = () => {
 
         <input
           type="text"
-          className="focus:border-b-1 block w-full border-b border-gray-300 py-1 pl-7 outline-none focus:border-blue-500 sm:pl-5"
+          className="block w-full border-b border-gray-300 py-1 pl-7 outline-none focus:border-blue-500 sm:pl-5"
           placeholder="タスクを入力"
+          defaultValue={value}
+          onChange={(e) => setValue(e.currentTarget.value)}
+          onKeyDown={onKeyDown}
+          maxLength={10}
         />
       </div>
     </div>
--- src/features/schedules/components/Items.tsx
+++ src/features/schedules/components/Items.tsx
@@ -71,22 +71,10 @@ const Items = (props: Props) => {
         </div>
       </div>
       <div className="py-2 pl-3 text-base font-bold text-gray-600 sm:text-xs">
-        {text.map((t) => (
-          <div key={t.text} className="flex items-center">
-            <div className="relative block">
-              <span className="absolute top-1/2 w-12 -translate-y-1/2 transform sm:h-5 sm:w-8">
-                {t.emoji}
-              </span>
-              <input
-                type="text"
-                className="focus:border-b-1 w-scree block border-b border-gray-300 py-1 pl-7 outline-none focus:border-blue-500 sm:pl-5"
-                placeholder="タスクを入力"
-                value={t.text}
-              />
-            </div>
-          </div>
+        {text.map((t, index) => (
+          <InputItem key={index} emoji={t.emoji} value={t.text} />
         ))}
-        <InputItem />
+        <InputItem input />
       </div>
     </div>
   );
