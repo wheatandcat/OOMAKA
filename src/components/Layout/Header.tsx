9905b7bba931ce5604b40e8b00784664ddd49714
wheatandcat <wheatandcat@gmail.com>
2023-10-01 18:57:46 +0900

    fix

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-
+                    
 const Header: React.FC = () => (
   <div className="container relative mx-auto flex max-w-screen-xl justify-between gap-12 px-4 ">
     <div className="py-2">
--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { exampleRouter } from "~/server/api/routers/example";
 import { scheduleRouter } from "~/server/api/routers/schedule";
 import { urlRouter } from "~/server/api/routers/url";
 import { createTRPCRouter } from "~/server/api/trpc";
@@ -9,7 +8,6 @@ import { createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   schedule: scheduleRouter,
   url: urlRouter,
 });
