8f1f4473b83bf6fc658ec427310ab7fe3d53a160
wheatandcat <wheatandcat@gmail.com>
2023-10-01 18:16:12 +0900

    fix

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
 
 const Header: React.FC = () => (
-  <div className="container mx-auto max-w-screen-xl gap-12 px-4 py-2">
-    <div className="mini-logo-title">年間スケジュール、まとめるなら</div>
-    <div className="logo-title text-2xl font-bold">OOMAKA</div>
+  <div className="container relative mx-auto flex max-w-screen-xl justify-between gap-12 px-4 ">
+    <div className="py-2">
+      <div className="mini-logo-title">年間スケジュール、まとめるなら</div>
+      <div className="logo-title text-2xl font-bold">OOMAKA</div>
+    </div>
   </div>
 );
 
