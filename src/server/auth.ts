e055e231b7c27f27b1e2bb49157790c23078fe77
wheatandcat <wheatandcat@gmail.com>
2023-11-12 13:42:14 +0900

    fix

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -79,6 +79,7 @@ export const authOptions: NextAuthOptions = {
      * @see https ://next-auth.js.org/providers/github
      */
   ],
+  secret: env.NEXTAUTH_SECRET,
   pages: {
     signIn: "/auth/signin",
     error: "/auth/signin",
