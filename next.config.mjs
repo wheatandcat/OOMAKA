a190a695bc8d97ec335809c1018611242cb1b73f
wheatandcat <wheatandcat@gmail.com>
2023-11-12 22:11:43 +0900

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -14,8 +14,8 @@ const config = {
    * @see https://github.com/vercel/next.js/issues/41980
    */
   i18n: {
-    locales: ["ja"],
-    defaultLocale: "ja",
+    locales: ["en"],
+    defaultLocale: "en",
   },
 };
 
